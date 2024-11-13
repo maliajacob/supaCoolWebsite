ce7045c00daef1d1e63b09c38dca65b01f85d80e
maliajacob <162858145+maliajacob@users.noreply.github.com>
2024-11-13 08:57:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function i() {
+    var i = document.getElementById("i");
+    i.style.display = "block";
+    var buttonone = document.getElementById("buttonone");
+    buttonone.style.display = "none";
+}
+function like() {
+    var like = document.getElementById("like");
+    like.style.display = "block";
+    var buttontwo = document.getElementById("buttontwo");
+    buttontwo.style.display = "none";
+}
+function u() {
+    var u = document.getElementById("you");
+    u.style.display = "block";
+    var buttonthree = document.getElementById("buttonthree");
+    buttonthree.style.display = "none";
+}
+function wanna() {
+    var wanna = document.getElementById("wanna");
+    wanna.style.display = "block";
+    var buttonfour = document.getElementById("buttonfour");
+    buttonfour.style.display = "none";
+}
+function go() {
+    var go = document.getElementById("go");
+    go.style.display = "block";
+    var buttonfive = document.getElementById("buttonfive");
+    buttonfive.style.display = "none";
+}
+function out() {
+    var out = document.getElementById("out");
+    out.style.display = "block";
+    var buttonsix = document.getElementById("buttonsix");
+    buttonsix.style.display = "none";
+}
+function im() {
+    var im = document.getElementById("im");
+    im.style.display = "block";
+    var buttonseven = document.getElementById("buttonseven");
+    buttonseven.style.display = "none";
+}
+function too() {
+    var too = document.getElementById("too");
+    too.style.display = "block";
+    var buttoneight = document.getElementById("buttoneight");
+    buttoneight.style.display = "none";
+}
+function anxious() {
+    var anxious = document.getElementById("anxious");
+    anxious.style.display = "block";
+    var buttonnine = document.getElementById("buttonnine");
+    buttonnine.style.display = "none";
+}
+function to() {
+    var to = document.getElementById("to");
+    to.style.display = "block";
+    var buttonten = document.getElementById("buttonten");
+    buttonten.style.display = "none";
+}
+function actually() {
+    var actually = document.getElementById("actually");
+    actually.style.display = "block";
+    var buttoneleven = document.getElementById("buttoneleven");
+    buttoneleven.style.display = "none";
+}
+function ask() {
+    var ask = document.getElementById("ask");
+    ask.style.display = "block";
+    var buttontwelve = document.getElementById("buttontwelve");
+    buttontwelve.style.display = "none";
+}
